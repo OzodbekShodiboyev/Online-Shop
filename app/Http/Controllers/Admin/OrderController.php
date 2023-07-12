69c0fcfaa9eb7ff1f1edcd8bfb503945716915ee
Javohir Mamaniyazov <118096306+javohirmamaniyazov@users.noreply.github.com>
2023-07-12 15:04:41 +0500

    Update OrderController.php index method

--- app/Http/Controllers/Admin/OrderController.php
+++ app/Http/Controllers/Admin/OrderController.php
@@ -13,22 +13,17 @@ class OrderController extends Controller
 {
     public function index(Request $request)
     {
-        $todayDate = Carbon::now()->format('Y-m-d');
         $orders = Order::when($request->date != null, function ($q) use ($request) {
-
             return $q->whereDate('created_at', $request->date);
-        }, function ($q) use ($todayDate) {
-
-            return $q->whereDate('created_at', $todayDate);
         })
-            ->when($request->status != null, function ($q) use ($request) {
-
-                return $q->where('status_message', $request->status);
-            })
-
-            ->paginate(10);
+        ->when($request->status != null, function ($q) use ($request) {
+            return $q->where('status_message', $request->status);
+        })
+        ->paginate(10);
+    
         return view('admin.orders.index', compact('orders'));
     }
+
     public function show(int $orderId)
     {
         $order = Order::where('id', $orderId)->first();
