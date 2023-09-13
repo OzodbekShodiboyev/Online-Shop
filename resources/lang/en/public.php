8ab642ecb7a4321ace49c4348e6b947b51774e7d
javohir <javohirmamaniyazov@gmail.com>
2023-09-13 09:00:51 +0500

    reuploading all of codes

--- resources/lang/en/public.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    'category' => 'Категории',
-    'main' => 'Домашняя страница',
-    'new_product' => 'Новые продукты',
-    // Add more translation keys and values as needed
-];
